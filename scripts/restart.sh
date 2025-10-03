235bccea17f1bb235dc4a747836f9f244501da44
hritiksagar01 <143049203hritiksagar01@users.noreply.github.com>
2025-10-03 23:12:49 +0530

    logs

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -4,6 +4,6 @@ mkdir -p /home/ubuntu/logs
 echo "===== restart.sh started at $(date) =====" >> $LOGFILE 2>&1
 
 echo "Restarting nginx..." >> $LOGFILE 2>&1
-systemctl restart nginx >> $LOGFILE 2>&1 || echo "nginx restart failed" >> $LOGFILE 2>&1
+systemctl restart nginx >> $LOGFILE 2>&1 || echo "nginx restart failedd" >> $LOGFILE 2>&1
 
 echo "Finished restart.sh at $(date)" >> $LOGFILE 2>&1
