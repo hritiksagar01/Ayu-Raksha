c11c6df067d12b64a8348866b392a01cfae7318f
hritiksagar01 <143049203hritiksagar01@users.noreply.github.com>
2025-10-03 21:58:28 +0530

    Make deploy scripts executable

--- scripts/copy_dist.sh
+++ scripts/copy_dist.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Remove old files from Nginx root
 sudo rm -rf /var/www/html/*
-# Copy the new build files
-sudo cp -r /home/ubuntu/Ayu-Raksha/dist/* /var/www/html/
 
-# Copy the new nginx config file
-sudo cp /home/ubuntu/Ayu-Raksha/ayu-raksha-app/nginx.conf /etc/nginx/conf.d/default.conf
+# Copy new build files from the deployment folder (current directory)
+sudo cp -r ./* /var/www/html/
